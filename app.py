966dda1e1059e7eec4c9c51ee4762fb753f4fd30
Sordy79 <111936348+Sordy79@users.noreply.github.com>
2026-09-25 07:43:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.markdown("""
     <p class="main-title">📱 Transcriptor en Tiempo Real (Estilo Galaxy S25)</p>
 """, unsafe_allow_html=True)
 
-col1, col2 = st.columns()
+col1, col2 = st.columns(2)
 
 with col1:
     st.markdown('<p class="section-label">🧏 Transcripción Instantánea (Voz a Texto):</p>', unsafe_allow_html=True)
